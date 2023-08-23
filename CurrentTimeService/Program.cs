f5f9d1d35a756f03d9283e5e5e8dedfaeb55bc4c
ali sadighi <ali@cwshost.com>
2023-08-23 09:48:09 -0700

    Added PDT time zone

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -11,6 +11,9 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 // GET UTC
-app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+app.MapGet("time/utc", () => Results.Ok(String.Format("UTC: {0}", DateTime.UtcNow)));
+TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+DateTime pacific = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzi);
+app.MapGet("time/local", () => Results.Ok(String.Format("PDT: {0}", pacific)));
 
 await app.RunAsync();
